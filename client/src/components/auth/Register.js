d39cd321baeece09581adab98151f3e6275b96e0
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2019-03-06 11:13:43 -0300

    Login Form Functionality

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import classnames from 'classnames';
-import { connect } from 'react-redux';
 import { registeruser } from '../../actions/authActions';
 
 class Register extends Component {
